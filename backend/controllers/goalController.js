3790ec488b218fb1f4d7bff093b5389f32c2227b
Aatman Vaidya <aatmanvaidya@gmail.com>
2022-10-28 22:57:37 +0530

    changes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,31 @@
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = (req,res) => {
+    res.status(200).json({message: 'Get Goals'})
+}
+
+// @desc Set goals
+// @route POST /api/goals
+// @access Private
+const setGoal = (req,res) => {
+    console.log(req.body)
+    res.status(200).json({message: 'Set Goals'})
+}
+
+// @desc Update goals
+// @route PUT /api/goals
+// @access Private
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+}
+ 
+// @desc Delete goals
+// @route DELETE /api/goals
+// @access Private
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+}
+module.exports = {
+    getGoals, setGoal, updateGoal, deleteGoal,
+}
